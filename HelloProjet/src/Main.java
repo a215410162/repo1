7ab898a3daa5fc471ffd8603b35c40efc1632b38
zhang <215410162@qq.com>
2018-12-16 18:51:31 +0800

    测试 idea集成git

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,8 @@
-import org.junit.Test;
 
 public class Main {
 
-    @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("测试");
     }
 }
