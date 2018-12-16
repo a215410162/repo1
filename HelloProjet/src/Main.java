4bdb88e5684acf07bbdca168e5539f484fcf25ec
zhang <215410162@qq.com>
2018-12-16 19:34:55 +0800

    测试 idea集成git

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("测试");
+        System.out.println("3333");
+        System.out.println("4444");
     }
 }
