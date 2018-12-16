712e80996282f78fbeaa52d561d5a178923f0e12
a215410162 <45909759+a215410162@users.noreply.github.com>
2018-12-16 19:36:03 +0800

    Update Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("测试");
-        System.out.println("3333");
-        System.out.println("4444");
+     
     }
 }
